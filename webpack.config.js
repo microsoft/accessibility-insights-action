5c3eec44132b9540f5984a557a85e8dd7a2ef313
Devaradhan Murugesan <demuruge@microsoft.com>
2020-02-12 17:31:42 -0800

    adding copyright check, linting, prettier & build support

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const path = require('path');
+const webpack = require('webpack');
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+
+module.exports = env => {
+    const version = env ? env.version : 'dev';
+    console.log(`Building for version : ${version}`);
+    return {
+        devtool: 'cheap-source-map',
+        entry: {
+            ['index']: path.resolve('./src/index.ts'),
+        },
+        mode: 'production',
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                transpileOnly: true,
+                                experimentalWatchApi: true,
+                            },
+                        },
+                    ],
+                    exclude: ['/node_modules/', /\.(spec|e2e)\.ts$/],
+                },
+            ],
+        },
+        name: 'scan-action',
+        node: {
+            __dirname: false,
+        },
+        output: {
+            path: path.resolve('./dist'),
+            filename: '[name].js',
+            libraryTarget: 'commonjs2',
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                __IMAGE_VERSION__: JSON.stringify(version),
+            }),
+            new ForkTsCheckerWebpackPlugin(),
+            new CaseSensitivePathsPlugin(),
+        ],
+        resolve: {
+            extensions: ['.ts', '.js', '.json'],
+            mainFields: ['main'], //This is fix for this issue https://www.gitmemory.com/issue/bitinn/node-fetch/450/494475397
+        },
+        target: 'node',
+    };
+};
