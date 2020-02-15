a79aec631bab110c8ab86e9e14d7cac5306492bb
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-02-14 17:12:04 -0800

    Scanner UTS

--- scanner/scanner.ts
+++ scanner/scanner.ts
@@ -1,12 +1,14 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 import { inject, injectable } from 'inversify';
+import * as url from 'url';
+
+import { iocTypes } from '../ioc/ioc-types';
+import { LocalFileServer } from '../local-file-server';
 import { Logger } from '../logger/logger';
 import { TaskConfig } from '../task-config';
-import { LocalFileServer } from '../local-file-server';
-import * as url from 'url';
 import { PromiseUtils } from '../utils/promise-utils';
+
 
 @injectable()
 export class Scanner {
@@ -15,12 +17,13 @@ export class Scanner {
         @inject(TaskConfig) private readonly taskConfig: TaskConfig,
         @inject(LocalFileServer) private readonly fileServer: LocalFileServer,
         @inject(PromiseUtils) private readonly promiseUtils: PromiseUtils,
+        @inject(iocTypes.Process)protected readonly currentProcess: typeof process,
     ) {}
 
     public async scan(): Promise<void> {
         await this.promiseUtils.waitFor(this.invokeScan(), 90000, async () => {
             this.logger.logError('Unable to scan before timeout');
-            process.exit(1);
+            this.currentProcess.exit(1);
         });
     }
 
