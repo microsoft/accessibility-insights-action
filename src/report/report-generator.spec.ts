dd2886abdc53312e5e90b9cb193c614305e37d3c
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2020-02-24 17:23:48 -0800

    fix uts

--- src/report/report-generator.spec.ts
+++ src/report/report-generator.spec.ts
@@ -19,7 +19,7 @@ describe('ReportGenerator', () => {
     let reportGenerator: ReportGenerator;
     const outputDir = 'outputDir';
     const scanUrl = 'https://www.bla.com';
-    const htmlReportString = 'outputDir/bla.com.html';
+    const htmlReportString = 'some html';
     let reporterMock: IMock<Reporter>;
     let htmlReport: Report;
     let axeResults: AxeResults;
@@ -27,15 +27,28 @@ describe('ReportGenerator', () => {
     let reportGenerationTime: Date;
     let loggerMock: IMock<Logger>;
     let taskConfigMock: IMock<TaskConfig>;
-    let filenamify: typeof filenamifyUrl = filenamifyUrl;
-    let fileSystemObj: typeof fs = fs;
+    let filenamifyMock: IMock<typeof filenamifyUrl>;
+    let fsMock: IMock<typeof fs>;
+    // tslint:disable-next-line:mocha-no-side-effect-code
+    const fileName = `${outputDir}/${filenamifyUrl(scanUrl, {
+        replacement: '_',
+    })}.html`;
 
     beforeEach(() => {
         loggerMock = Mock.ofType<Logger>();
         taskConfigMock = Mock.ofType<TaskConfig>();
         reporterMock = Mock.ofType<Reporter>();
+        filenamifyMock = Mock.ofType<typeof filenamifyUrl>();
+        fsMock = Mock.ofType<typeof fs>();
         const reporterFactory: ReporterFactory = () => reporterMock.object;
-        reportGenerator = new ReportGenerator(taskConfigMock.object, reporterFactory, loggerMock.object);
+
+        reportGenerator = new ReportGenerator(
+            taskConfigMock.object,
+            reporterFactory,
+            loggerMock.object,
+            filenamifyMock.object,
+            fsMock.object,
+        );
         htmlReport = {
             asHTML: () => htmlReportString,
         };
@@ -43,7 +56,7 @@ describe('ReportGenerator', () => {
             url: scanUrl,
         } as unknown) as AxeResults;
 
-        axeScanResults = { results: axeResults, pageTitle: 'page title', browserSpec: 'browser version' };
+        axeScanResults = { results: axeResults, pageTitle: 'page title', browserSpec: 'browser version' } as AxeScanResults;
         reportGenerationTime = new Date(2019, 2, 3);
         MockDate.set(reportGenerationTime);
         taskConfigMock
@@ -52,7 +65,7 @@ describe('ReportGenerator', () => {
             .verifiable(Times.once());
     });
 
-    it('generate report ', () => {
+    test.each([true, false])('generate report', (directoryExists: boolean) => {
         const params = {
             pageTitle: axeScanResults.pageTitle,
         };
@@ -71,9 +84,23 @@ describe('ReportGenerator', () => {
             .returns(() => htmlReport)
             .verifiable(Times.once());
 
-        const report = reportGenerator.generateReport(axeScanResults);
+        fsMock
+            .setup(fsm => fsm.existsSync(outputDir))
+            .returns(() => directoryExists)
+            .verifiable(Times.once());
+
+        fsMock
+            .setup(fsm => fsm.writeFileSync(fileName, htmlReportString))
+            .returns(() => true)
+            .verifiable(Times.once());
+
+        if (!directoryExists) {
+            fsMock.setup(fsm => fsm.mkdirSync(outputDir)).verifiable(Times.once());
+        }
+
+        reportGenerator.generateReport(axeScanResults);
 
         reporterMock.verifyAll();
-        expect(report).toEqual(htmlReportString);
     });
+
 });
