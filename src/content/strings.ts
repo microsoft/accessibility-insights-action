6075608894e8c9b894859dc3644c98726c84bd3c
Haonan Liu <haonliu@microsoft.com>
2020-02-26 12:02:26 -0800

    remove '

--- src/content/strings.ts
+++ src/content/strings.ts
@@ -7,4 +7,4 @@ export const toolName = title;
 export const brandLogoImg = 'https://accessibilityinsights.io/img/a11yinsights-blue.svg';
 export const checkRunName = 'Accessibility Checks';
 export const checkRunDetailsTitle = `Accessibility Automated Checks Results`;
-export const checkRunSummaryMd = `The Accessibility Insights Service ran a set of automated checks to help find some of the most common accessibility issues. The best way to evaluate web accessibility compliance is to complete a ['WCAG 2.1 compliance assessment'](http://aka.ms/AccessibilityInsights).`;
+export const checkRunSummaryMd = `The Accessibility Insights Service ran a set of automated checks to help find some of the most common accessibility issues. The best way to evaluate web accessibility compliance is to complete a [WCAG 2.1 compliance assessment](http://aka.ms/AccessibilityInsights).`;
