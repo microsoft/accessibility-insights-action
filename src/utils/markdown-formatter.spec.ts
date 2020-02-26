e7716a764b67d9a31675f592770d130168b86231
Haonan Liu <haonliu@microsoft.com>
2020-02-25 16:59:00 -0800

    add uts for formatter

---
+++ src/utils/markdown-formatter.spec.ts
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+import {
+    bold,
+    footerSeparator,
+    heading,
+    image,
+    link,
+    listItem,
+    productTitle,
+    sectionSeparator,
+    snippet,
+} from './markdown-formatter';
+
+describe('MarkdownFormatter', () => {
+    it('snippet', () => {
+        expect(snippet('code')).toMatchSnapshot();
+    });
+
+    it('link', () => {
+        expect(link('href', 'text')).toMatchSnapshot();
+    });
+
+    it('image', () => {
+        expect(image('alt', 'src')).toMatchSnapshot();
+    });
+
+    it('listItem', () => {
+        expect(listItem('li')).toMatchSnapshot();
+    });
+
+    it('heading', () => {
+        expect(heading('heading', 2)).toMatchSnapshot();
+    });
+
+    it('bold', () => {
+        expect(bold('text')).toMatchSnapshot();
+    });
+
+    it('productTitle', () => {
+        expect(productTitle()).toMatchSnapshot();
+    });
+
+    it('footerSeparator', () => {
+        expect(footerSeparator()).toMatchSnapshot();
+    });
+
+    it('sectionSeparator', () => {
+        expect(sectionSeparator()).toMatchSnapshot();
+    });
+});
