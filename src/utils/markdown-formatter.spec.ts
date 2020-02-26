70e9e95df0d5f29badaa9101913608649463c6f4
Haonan Liu <haonliu@microsoft.com>
2020-02-25 16:59:14 -0800

    format

--- src/utils/markdown-formatter.spec.ts
+++ src/utils/markdown-formatter.spec.ts
@@ -1,16 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-import {
-    bold,
-    footerSeparator,
-    heading,
-    image,
-    link,
-    listItem,
-    productTitle,
-    sectionSeparator,
-    snippet,
-} from './markdown-formatter';
+import { bold, footerSeparator, heading, image, link, listItem, productTitle, sectionSeparator, snippet } from './markdown-formatter';
 
 describe('MarkdownFormatter', () => {
     it('snippet', () => {
