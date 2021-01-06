microsoft/accessibility-insights-action

commit 5ee3a20415e3c775bdbb74b4759d7eb088092626

fix: avoid bundling MPL dependencies into same file as non-MPL code (#395)
#### Description of changes

This updates our build process per CELA guidance to avoid bundling MPL code into the same files as non-MPL code for distribution.

#### Pull request checklist

- [x] Addresses an existing issue: 1797828
- [x] Added relevant unit test for your changes. (`yarn test`)
- [x] Verified code coverage for the changes made. Check coverage report at: `<rootDir>/test-results/unit/coverage`
- [x] Ran precheckin (`yarn precheckin`)

src/scanner/scanner.ts
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 import { AIScanner } from 'accessibility-insights-scan';
 import { inject, injectable } from 'inversify';
-import * as path from 'path';
 import * as url from 'url';
 import * as util from 'util';
 
@@ -46,7 +45,7 @@ export class Scanner {
             this.logger.logInfo(`Starting accessibility scanning of URL ${scanUrl}.`);
 
             const chromePath = this.taskConfig.getChromePath();
-            const axeCoreSourcePath = path.resolve(__dirname, 'axe.js');
+            const axeCoreSourcePath = require.resolve('axe-core/axe.min.js');
 
             const axeScanResults = await this.scanner.scan(scanUrl, chromePath, axeCoreSourcePath);
 

src/scanner/scanner.spec.ts
@@ -3,7 +3,6 @@
 import 'reflect-metadata';
 
 import { AIScanner, AxeScanResults } from 'accessibility-insights-scan';
-import * as path from 'path';
 import { IMock, It, Mock, Times } from 'typemoq';
 import * as util from 'util';
 import { LocalFileServer } from '../local-file-server';
@@ -28,7 +27,7 @@ describe(Scanner, () => {
     let axeScanResults: AxeScanResults;
     const scanUrl = 'localhost';
     const baseUrl = 'base';
-    const axeSourcePath = path.resolve(__dirname, 'axe.js');
+    const axeSourcePath = require.resolve('axe-core/axe.min.js');
     const chromePath = 'chrome path';
 
     beforeEach(() => {

.gitignore
@@ -2,6 +2,7 @@
 # Licensed under the MIT License.
 
 node_modules/
+!/dist/node_modules/
 drop/
 test-results/
 out/

webpack.config.js
@@ -16,6 +16,10 @@ module.exports = (env) => {
         entry: {
             ['index']: path.resolve('./src/index.ts'),
         },
+        // We special case MPL-licensed deps because we want to avoid including their source in
+        // the same file as non-MPL code. Note that each entry here should have a corresponding
+        // entry in copyWebpackPlugin config to copy the non-bundled forms to /dist.
+        externals: ['axe-core', '@axe-core/puppeteer'],
         mode: 'development',
         module: {
             rules: [
@@ -51,11 +55,8 @@ module.exports = (env) => {
             new CaseSensitivePathsPlugin(),
             new copyWebpackPlugin({
                 patterns: [
-                    {
-                        context: './',
-                        from: 'node_modules/axe-core/axe.min.js',
-                        to: 'axe.js',
-                    },
+                    `node_modules/axe-core/*`, // we only use the root-level axe.min.js and the package metadata/LICENSE stuff
+                    `node_modules/@axe-core/puppeteer/**/*`,
                 ],
             }),
         ],

commit f43a2391a67278a60ff58e55186a400d3ed8ca42

fix: prevent error when injecting axe.min.js introduced in #395 (#399)
#### Description of changes

Fixes a bug introduced in #395 where resolving axe.min.js doesn't work correctly when the bundled index.js is run from a working directory that isn't its own directory (because `require.resolve` returns a relative path as #395 used it).

#### Pull request checklist

- [n/a] Addresses an existing issue: Fixes #0000
- [x] Added relevant unit test for your changes. (`yarn test`)
- [x] Verified code coverage for the changes made. Check coverage report at: `<rootDir>/test-results/unit/coverage`
- [x] Ran precheckin (`yarn precheckin`)

src/scanner/scanner.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 import { AIScanner } from 'accessibility-insights-scan';
 import { inject, injectable } from 'inversify';
+import * as path from 'path';
 import * as url from 'url';
 import * as util from 'util';
 
@@ -45,7 +46,8 @@ export class Scanner {
             this.logger.logInfo(`Starting accessibility scanning of URL ${scanUrl}.`);
 
             const chromePath = this.taskConfig.getChromePath();
-            const axeCoreSourcePath = require.resolve('axe-core/axe.min.js');
+            // Note: this is relative to /dist/index.js, not this source file
+            const axeCoreSourcePath = path.resolve(__dirname, 'node_modules', 'axe-core', 'axe.min.js');
 
             const axeScanResults = await this.scanner.scan(scanUrl, chromePath, axeCoreSourcePath);
 

src/scanner/scanner.spec.ts
@@ -3,6 +3,7 @@
 import 'reflect-metadata';
 
 import { AIScanner, AxeScanResults } from 'accessibility-insights-scan';
+import * as path from 'path';
 import { IMock, It, Mock, Times } from 'typemoq';
 import * as util from 'util';
 import { LocalFileServer } from '../local-file-server';
@@ -27,7 +28,7 @@ describe(Scanner, () => {
     let axeScanResults: AxeScanResults;
     const scanUrl = 'localhost';
     const baseUrl = 'base';
-    const axeSourcePath = require.resolve('axe-core/axe.min.js');
+    const axeSourcePath = path.resolve(__dirname, 'node_modules', 'axe-core', 'axe.min.js');
     const chromePath = 'chrome path';
 
     beforeEach(() => {