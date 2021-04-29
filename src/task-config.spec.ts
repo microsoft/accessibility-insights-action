b85a7c809cd7f9f154d475bbb078aa2e7f0509ac
karan <karansin@microsoft.com>
2021-04-28 19:03:42 -0700

    add arrow functions to avoid unbounded this lint error

--- src/task-config.spec.ts
+++ src/task-config.spec.ts
@@ -23,24 +23,27 @@ describe(TaskConfig, () => {
 
     it.each`
         inputOption                 | inputValue   | expectedValue       | getInputFunc
-        ${'repo-token'}             | ${'token'}   | ${'token'}          | ${taskConfig.getToken}
-        ${'scan-url-relative-path'} | ${'path'}    | ${'path'}           | ${taskConfig.getScanUrlRelativePath}
-        ${'chrome-path'}            | ${'path'}    | ${'path'}           | ${taskConfig.getChromePath}
-        ${'input-file'}             | ${'path'}    | ${'path'}           | ${taskConfig.getInputFile}
-        ${'output-dir'}             | ${'path'}    | ${'path'}           | ${taskConfig.getReportOutDir}
-        ${'site-dir'}               | ${'path'}    | ${'path'}           | ${taskConfig.getSiteDir}
-        ${'url'}                    | ${'url'}     | ${'url'}            | ${taskConfig.getUrl}
-        ${'discovery-patterns'}     | ${'a bb  c'} | ${['a', 'bb', 'c']} | ${taskConfig.getDiscoveryPatterns}
-        ${'input-urls'}             | ${'a bb  c'} | ${['a', 'bb', 'c']} | ${taskConfig.getInputUrls}
-        ${'max-urls'}               | ${'20'}      | ${20}               | ${taskConfig.getMaxUrls}
-    `('$getInputFunc provides parsed version of $inputOption', ({ inputOption, getInputFunc, inputValue, expectedValue }) => {
-        actionCoreMock
-            .setup((am) => am.getInput(inputOption))
-            .returns(() => inputValue)
-            .verifiable(Times.once());
-        const retrievedOption = getInputFunc.call(taskConfig);
-        expect(retrievedOption).toStrictEqual(expectedValue);
-    });
+        ${'repo-token'}             | ${'token'}   | ${'token'}          | ${() => taskConfig.getToken()}
+        ${'scan-url-relative-path'} | ${'path'}    | ${'path'}           | ${() => taskConfig.getScanUrlRelativePath()}
+        ${'chrome-path'}            | ${'path'}    | ${'path'}           | ${() => taskConfig.getChromePath()}
+        ${'input-file'}             | ${'path'}    | ${'path'}           | ${() => taskConfig.getInputFile()}
+        ${'output-dir'}             | ${'path'}    | ${'path'}           | ${() => taskConfig.getReportOutDir()}
+        ${'site-dir'}               | ${'path'}    | ${'path'}           | ${() => taskConfig.getSiteDir()}
+        ${'url'}                    | ${'url'}     | ${'url'}            | ${() => taskConfig.getUrl()}
+        ${'discovery-patterns'}     | ${'a bb  c'} | ${['a', 'bb', 'c']} | ${() => taskConfig.getDiscoveryPatterns()}
+        ${'input-urls'}             | ${'a bb  c'} | ${['a', 'bb', 'c']} | ${() => taskConfig.getInputUrls()}
+        ${'max-urls'}               | ${'20'}      | ${20}               | ${() => taskConfig.getMaxUrls()}
+    `(
+        'parses "$inputValue" as $expectedValue when provided as $inputOption',
+        ({ inputOption, getInputFunc, inputValue, expectedValue }) => {
+            actionCoreMock
+                .setup((am) => am.getInput(inputOption))
+                .returns(() => inputValue)
+                .verifiable(Times.once());
+            const retrievedOption = getInputFunc();
+            expect(retrievedOption).toStrictEqual(expectedValue);
+        },
+    );
 
     it('getChromePath returns empty', () => {
         const chromePath = 'some path';
