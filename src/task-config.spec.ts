65ba6c46d740f8605fffe0583d9ab93e663b9e7a
karan <karansin@microsoft.com>
2021-04-28 18:43:20 -0700

    Add CLI options to action input

--- src/task-config.spec.ts
+++ src/task-config.spec.ts
@@ -4,89 +4,42 @@ import 'reflect-metadata';
 
 import * as actionCore from '@actions/core';
 import * as process from 'process';
-import { IMock, Mock, Times } from 'typemoq';
+import { Mock, Times } from 'typemoq';
 
 import { TaskConfig } from './task-config';
 
 describe(TaskConfig, () => {
-    let processStub: typeof process;
-    let actionCoreMock: IMock<typeof actionCore>;
-    let taskConfig: TaskConfig;
     const runId = 789;
     const workspace = 'some-workspace';
+    const processStub = {
+        env: {
+            GITHUB_WORKSPACE: workspace,
+            GITHUB_RUN_ID: `${runId}`,
+        },
+    } as any;
 
-    beforeEach(() => {
-        processStub = {
-            env: {
-                GITHUB_WORKSPACE: workspace,
-                GITHUB_RUN_ID: `${runId}`,
-            },
-        } as any;
-        actionCoreMock = Mock.ofType<typeof actionCore>();
+    const actionCoreMock = Mock.ofType<typeof actionCore>();
+    const taskConfig = new TaskConfig(processStub, actionCoreMock.object);
 
-        taskConfig = new TaskConfig(processStub, actionCoreMock.object);
-    });
-
-    it('getReportOutDir', () => {
-        const outputDir = 'output-dir';
+    it.each`
+        inputOption                 | inputValue   | expectedValue       | getInputFunc
+        ${'repo-token'}             | ${'token'}   | ${'token'}          | ${taskConfig.getToken}
+        ${'scan-url-relative-path'} | ${'path'}    | ${'path'}           | ${taskConfig.getScanUrlRelativePath}
+        ${'chrome-path'}            | ${'path'}    | ${'path'}           | ${taskConfig.getChromePath}
+        ${'input-file'}             | ${'path'}    | ${'path'}           | ${taskConfig.getInputFile}
+        ${'output-dir'}             | ${'path'}    | ${'path'}           | ${taskConfig.getReportOutDir}
+        ${'site-dir'}               | ${'path'}    | ${'path'}           | ${taskConfig.getSiteDir}
+        ${'url'}                    | ${'url'}     | ${'url'}            | ${taskConfig.getUrl}
+        ${'discovery-patterns'}     | ${'a bb  c'} | ${['a', 'bb', 'c']} | ${taskConfig.getDiscoveryPatterns}
+        ${'input-urls'}             | ${'a bb  c'} | ${['a', 'bb', 'c']} | ${taskConfig.getInputUrls}
+        ${'max-urls'}               | ${'20'}      | ${20}               | ${taskConfig.getMaxUrls}
+    `('$getInputFunc provides parsed version of $inputOption', ({ inputOption, getInputFunc, inputValue, expectedValue }) => {
         actionCoreMock
-            .setup((am) => am.getInput('output-dir'))
-            .returns(() => outputDir)
+            .setup((am) => am.getInput(inputOption))
+            .returns(() => inputValue)
             .verifiable(Times.once());
-        const dir = taskConfig.getReportOutDir();
-        expect(dir).toBe(outputDir);
-    });
-
-    it('getSiteDir', () => {
-        const siteDir = 'site';
-        actionCoreMock
-            .setup((am) => am.getInput('site-dir'))
-            .returns(() => siteDir)
-            .verifiable(Times.once());
-
-        const dir = taskConfig.getSiteDir();
-
-        expect(dir).toBe(siteDir);
-        actionCoreMock.verifyAll();
-    });
-
-    it('getScanUrlRelativePath', () => {
-        const relativePath = 'path';
-        actionCoreMock
-            .setup((am) => am.getInput('scan-url-relative-path'))
-            .returns(() => relativePath)
-            .verifiable(Times.once());
-
-        const res = taskConfig.getScanUrlRelativePath();
-
-        expect(res).toBe(relativePath);
-        actionCoreMock.verifyAll();
-    });
-
-    it('getToken', () => {
-        const token = 'repo-token';
-        actionCoreMock
-            .setup((am) => am.getInput('repo-token'))
-            .returns(() => token)
-            .verifiable(Times.once());
-
-        const res = taskConfig.getToken();
-
-        expect(res).toBe(token);
-        actionCoreMock.verifyAll();
-    });
-
-    it('getChromePath', () => {
-        const chromePath = 'chrome-path';
-        actionCoreMock
-            .setup((am) => am.getInput('chrome-path'))
-            .returns(() => chromePath)
-            .verifiable(Times.once());
-
-        const res = taskConfig.getChromePath();
-
-        expect(res).toBe(chromePath);
-        actionCoreMock.verifyAll();
+        const retrievedOption = getInputFunc.call(taskConfig);
+        expect(retrievedOption).toStrictEqual(expectedValue);
     });
 
     it('getChromePath returns empty', () => {
@@ -101,26 +54,16 @@ describe(TaskConfig, () => {
         const res = taskConfig.getChromePath();
 
         expect(res).toBe(chromePath);
-        actionCoreMock.verifyAll();
-    });
-
-    it('getUrl', () => {
-        const remoteUrl = 'remote-url';
-        actionCoreMock
-            .setup((am) => am.getInput('url'))
-            .returns(() => remoteUrl)
-            .verifiable(Times.once());
-
-        const res = taskConfig.getUrl();
-
-        expect(res).toBe(remoteUrl);
-        actionCoreMock.verifyAll();
     });
 
     it('getRunId', () => {
         const res = taskConfig.getRunId();
 
         expect(res).toBe(runId);
+    });
+
+    afterEach(() => {
         actionCoreMock.verifyAll();
+        actionCoreMock.reset();
     });
 });
