588b37e97dc5127561f7e2401c3647dd98f7734e
Maxim Laikine <40576301+lamaks@users.noreply.github.com>
2021-04-30 14:28:38 -0700

    Update docker setup script (#572)
    * Fix docker setup
    
    * Fix formatting
    
    * Remove unused npm references
    
    * Merge with main
    
    * Fix Windows path
    
    * Revert "Fix Windows path"
    
    This reverts commit 486a74f7f4335ac204d7fed1b8e01428e54eda74.
    
    * Fix windows path

--- src/scanner/scanner.ts
+++ src/scanner/scanner.ts
@@ -57,7 +57,7 @@ export class Scanner {
             this.logger.logInfo(`Starting accessibility scanning of URL ${scanUrl}`);
 
             const chromePath = this.taskConfig.getChromePath();
-            this.logger.logInfo(`Chrome app executable ${chromePath}`);
+            this.logger.logInfo(`Chrome app executable: ${chromePath ?? 'system default'}`);
 
             // Note: this is relative to /dist/index.js, not this source file
             const axeCoreSourcePath = path.resolve(__dirname, 'node_modules', 'axe-core', 'axe.js');
