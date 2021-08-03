c25a385f3353862e258d1a35db0088a34db853c3
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2021-08-02 17:18:13 -0700

    remove run

--- release.sh
+++ release.sh
@@ -27,7 +27,7 @@ echo "delete old node_modules"
 rm -rf node_modules
 
 echo "install external dependencies"
-run: yarn install --frozen-lockfile
+yarn install --frozen-lockfile
 
 echo "delete pupetter chromium folder"
 rm -rf node_modules/puppeteer/.local-chromium
