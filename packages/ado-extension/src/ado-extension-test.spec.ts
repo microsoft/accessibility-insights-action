cea8f17fd7a0b836237a3c64020b2d6b1325d580
Jacqueline Gibson <jagibson@microsoft.com>
2021-08-10 09:09:34 -0700

    added jest.config file and the placeholder test file for ado package

---
+++ packages/ado-extension/src/ado-extension-test.spec.ts
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+describe('ADO Extension test infrastructure', () => {
+    it('placeholder test in ADO extension wrapper package', () => {
+        expect(0).toBe(0);
+    });
+});
