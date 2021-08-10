8ac014f61b9095ec0456212c8aaa08ca68245042
Jacqueline Gibson <jagibson@microsoft.com>
2021-08-10 09:23:34 -0700

    I've created a change

--- packages/gh-action/src/gh-action-test.spec.ts
+++ packages/gh-action/src/gh-action-test.spec.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License.
 
 describe('GH Action test infrastructure', () => {
-    it('placeholder test in GitHub action wrapper package', () => {
+    it('placeholder test in GitHub Action wrapper package. Woot', () => {
         expect(0).toBe(0);
     });
 });
