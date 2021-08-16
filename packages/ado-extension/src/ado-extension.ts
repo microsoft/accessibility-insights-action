0129f92f4d8f02c07e4054d31988bce37ad2f42b
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2021-08-16 15:41:23 -0700

    remove unused import

--- packages/ado-extension/src/ado-extension.ts
+++ packages/ado-extension/src/ado-extension.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import * as adoTask from 'azure-pipelines-task-lib/task';
-
 try {
     const url = process.argv.slice(2)[0];
     console.log(`Scanning ${url}`);
