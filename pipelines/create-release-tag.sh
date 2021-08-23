4e43c66965054b944620f7542f67ab1552f5b1ef
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2021-08-23 07:42:44 -0700

    download artifacts

--- pipelines/create-release-tag.sh
+++ pipelines/create-release-tag.sh
@@ -1,24 +1,18 @@
-echo 'cloning the repo'
-git clone https://$(repo-token)@$(repo)
-
-echo 'cd to repo folder'
-cd accessibility-insights-action
-
 echo "Setting user name and email"
 git config --global user.name $(user-name)
 git config --global user.email $(email)
 
 echo 'create a new branch to use for creating the release'
-git checkout -b releases/gh/$(tag)
+git checkout -b releases/ado/$(tag)
 
 echo 'delete all files in the repo'
 git rm -rf .
 
 echo 'copy drop artifacts to the branch'
-cp -r '$(System.DefaultWorkingDirectory)/artifactFolder/ado-extension-drop/NOTICE.html' '$(System.DefaultWorkingDirectory)/accessibility-insights-action/'
-cp -r '$(System.DefaultWorkingDirectory)/artifactFolder/ado-extension-drop/ado-extension.json' '$(System.DefaultWorkingDirectory)/accessibility-insights-action/'
-cp -r '$(System.DefaultWorkingDirectory)/artifactFolder/ado-extension-drop/pkg' '$(System.DefaultWorkingDirectory)/accessibility-insights-action/pkg'
-cp -r '$(System.DefaultWorkingDirectory)/artifactFolder/ado-extension-drop/drop/RELEASE_COMMIT.md' '$(System.DefaultWorkingDirectory)/accessibility-insights-action/README.md'
+cp -r '$(System.DefaultWorkingDirectory)/ado-extension-drop/NOTICE.html' '$(System.DefaultWorkingDirectory)/accessibility-insights-action/'
+cp -r '$(System.DefaultWorkingDirectory)/ado-extension-drop/ado-extension.json' '$(System.DefaultWorkingDirectory)/accessibility-insights-action/'
+cp -r '$(System.DefaultWorkingDirectory)/ado-extension-drop/pkg' '$(System.DefaultWorkingDirectory)/accessibility-insights-action/pkg'
+cp -r '$(System.DefaultWorkingDirectory)/ado-extension-drop/drop/RELEASE_COMMIT.md' '$(System.DefaultWorkingDirectory)/accessibility-insights-action/README.md'
 
 echo 'git add'
 git add .
