5ac5fa0834b703f519e4e06633038d2eec6e3307
Ahme Abdo Ahmed Abdo Mohamed <ahmohame@microsoft.com>
2021-08-23 08:05:04 -0700

    fix env

--- pipelines/create-release-tag.sh
+++ pipelines/create-release-tag.sh
@@ -1,9 +1,9 @@
 echo "Setting user name and email"
-git config --global user.name $(user)
-git config --global user.email $(email)
+git config --global user.name $USER
+git config --global user.email $EMAIL
 
 echo 'create a new branch to use for creating the release'
-git checkout -b releases/ado/$(tag)
+git checkout -b releases/ado/$TAG
 
 echo 'delete all files in the repo'
 git rm -rf .
@@ -21,7 +21,7 @@ echo 'git commit'
 git commit --allow-empty -m "root commit"
 
 echo 'create release tag'
-git tag $(tag)
+git tag $TAG
 
 echo 'push the release tag'
-git push origin $(tag)
+git push origin $TAG
