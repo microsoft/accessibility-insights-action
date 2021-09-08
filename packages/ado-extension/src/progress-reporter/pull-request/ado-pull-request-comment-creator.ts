c2bf8b150231c272de2500598ec95898d88e394a
John Alkire <john.alkire@microsoft.com>
2021-09-08 16:00:09 -0700

    Fix issues from strictNullChecks merge

--- packages/ado-extension/src/progress-reporter/pull-request/ado-pull-request-comment-creator.ts
+++ packages/ado-extension/src/progress-reporter/pull-request/ado-pull-request-comment-creator.ts
@@ -29,24 +29,37 @@ export class AdoPullRequestCommentCreator extends ProgressReporter {
             return;
         }
 
-        let token = '';
+        let token: string;
 
         const serviceConnectionName = adoTaskConfig.getRepoServiceConnectionName();
-        if (serviceConnectionName?.length > 0) {
-            const userProvidedServiceConnection = adoTask.getEndpointAuthorization(serviceConnectionName, true);
+        if (serviceConnectionName !== undefined && serviceConnectionName?.length > 0) {
+            // Will throw if no creds found
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            const userProvidedServiceConnection = adoTask.getEndpointAuthorization(serviceConnectionName, false)!;
+
             // We should check the different schemes supported and access the appropriate params. Here we assume it's Token-based
             // https://docs.microsoft.com/en-us/azure/devops/extend/develop/auth-schemes?view=azure-devops
             token = userProvidedServiceConnection.parameters['apitoken'];
             console.log('Using token provided by service connection passed in by user');
         } else {
-            // falling back to build agent default creds
-            token = adoTask.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', true);
+            // falling back to build agent default creds. Will throw if no creds found
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            token = adoTask.getEndpointAuthorizationParameter('SystemVssConnection', 'AccessToken', false)!;
             console.log('Could not find a service connection passed in by the user. Trying to use default build agent creds');
         }
 
         const authHandler = nodeApi.getPersonalAccessTokenHandler(token);
-        const url = adoTask.getVariable('System.TeamFoundationCollectionUri');
+        const url = this.getVariableOrThrow('System.TeamFoundationCollectionUri');
         this.connection = new nodeApi.WebApi(url, authHandler);
+    }
+
+    private getVariableOrThrow(variableName: string): string {
+        const potentialVariable = this.adoTask.getVariable(variableName);
+        if (potentialVariable === undefined)
+        {
+            throw `Unable to find ${variableName}`;
+        }
+        return potentialVariable;
     }
 
     public async start(): Promise<void> {
@@ -61,14 +74,14 @@ export class AdoPullRequestCommentCreator extends ProgressReporter {
         const reportMarkdown = this.reportMarkdownConvertor.convert(combinedReportResult);
         this.traceMarkdown(reportMarkdown);
 
-        const prId = parseInt(this.adoTask.getVariable('System.PullRequest.PullRequestId'));
-        const repoId = this.adoTask.getVariable('Build.Repository.ID');
+        const prId = parseInt(this.getVariableOrThrow('System.PullRequest.PullRequestId'));
+        const repoId = this.getVariableOrThrow('Build.Repository.ID');
         this.logMessage(`PR is ${prId}, repo is ${repoId}`);
 
         const gitApiObject: GitApi.IGitApi = await this.connection.getGitApi();
         const prThreads = await gitApiObject.getThreads(repoId, prId);
-        const existingThread = prThreads.find((p) => p.comments.some((c) => c.content.includes('Accessibility Insights')));
-        if (existingThread == undefined) {
+        const existingThread = prThreads.find((p) => p.comments?.some((c) => c.content?.includes('Accessibility Insights')));
+        if (existingThread === undefined || existingThread.id === undefined) {
             this.logMessage(`Didn't find an existing thread, making a new thread`);
             await gitApiObject.createThread(
                 {
