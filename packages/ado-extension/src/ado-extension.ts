f52e74aa344bda8ad49d0d8afc03ad0980aa628a
Lisa Li <lisli@microsoft.com>
2021-09-08 14:32:38 -0700

    put ! on the right line

--- packages/ado-extension/src/ado-extension.ts
+++ packages/ado-extension/src/ado-extension.ts
@@ -5,9 +5,9 @@ import * as adoTask from 'azure-pipelines-task-lib/task';
 
 export function runScan() {
     try {
-        const url = adoTask.getInput('url', true);
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        console.log(`Scanning ${url!}`);
+        const url = adoTask.getInput('url', true)!;
+        console.log(`Scanning ${url}`);
     } catch (error) {
         console.log('Exception thrown in action: ', error);
         process.exit(1);
