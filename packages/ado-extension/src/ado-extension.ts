1bd55294f72ffe9b7c5de08db22b8c492670996e
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-02-03 14:14:32 -0800

    Straight from repo, doesn't compile

--- packages/ado-extension/src/ado-extension.ts
+++ packages/ado-extension/src/ado-extension.ts
@@ -1,6 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable prefer-rest-params */
+
 import 'reflect-metadata';
 import './module-name-mapper';
 
@@ -10,6 +16,34 @@ import { setupIocContainer } from './ioc/setup-ioc-container';
 
 export function runScan() {
     (async () => {
+
+        // function hookOutputStream(stream: NodeJS.WriteStream, callback: any) {
+        //     const old_write = stream.write;
+          
+        //     console.log('DHT = input = ')
+        //     const y = function (buffer: string | Uint8Array, cb?: ((err?: Error | undefined) => void) | undefined): boolean || (str: string | Uint8Array, encoding?: BufferEncoding | undefined, cb?: ((err?: Error | undefined) => void) | undefined): boolean) {}
+
+        //     const x = function(buffer: any, encoding: any, fd: any) {
+        //         const updatedString = callback(buffer);
+
+        //         if (updatedString) {
+        //             old_write(updatedString, encoding, fd);
+        //         }
+        //     }
+
+        //     stream.write = x;
+        // }
+
+        // // hookOutputStream(process.stdout, function(buffer: string): string | null {
+        // //     return buffer;
+        // // });
+
+        // hookOutputStream(process.stderr, function(buffer: string): string | null {
+        //     if (buffer.startsWith('waitFor')) return null;
+
+        //     return buffer;
+        // });
+
         const container = setupIocContainer();
         const logger = container.get(Logger);
         await logger.setup();
