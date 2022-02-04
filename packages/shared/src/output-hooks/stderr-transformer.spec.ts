42ac548f98d80534f0bc14bc24f14bdb3ea2cbc7
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-02-04 09:50:33 -0800

    add tests for stderr-transformer

---
+++ packages/shared/src/output-hooks/stderr-transformer.spec.ts
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { stderrTransformer } from './stderr-transformer';
+
+describe(stderrTransformer, () => {
+    it.each`
+        input                                                                   | expectedOutput
+        ${'abc'}                                                                | ${'abc'}}
+        ${'waitFor is deprecate'}                                               | ${'waitFor is deprecate'}}
+        ${'waitFor is deprecated'}                                              | ${null}}
+        ${'waitFor is deprecated abc'}                                          | ${null}}
+        ${'Warning: Each child in a list should have a unique "key" pro'}       | ${'Warning: Each child in a list should have a unique "key" pro'}}
+        ${'Warning: Each child in a list should have a unique "key" prop'}      | ${null}}
+        ${'Warning: Each child in a list should have a unique "key" prop abc'}  | ${null}}
+    `(
+        `input value '$input' returned as '$expectedOutput'`,
+        ({ input, expectedOutput }) => {
+            const output = stderrTransformer(input);
+            expect(output).toBe(expectedOutput);
+        },
+    );
+});
