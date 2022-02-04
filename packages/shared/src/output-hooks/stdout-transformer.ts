1e29c206f4db4d5c04ff0b98f39445fc33cb002f
Dave Tryon <45672944+DaveTryon@users.noreply.github.com>
2022-02-03 17:11:43 -0800

    minor cleanup

--- packages/shared/src/output-hooks/stdout-transformer.ts
+++ packages/shared/src/output-hooks/stdout-transformer.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { stderr } from 'process';
-
 type StartSubstitution = {
     oldStart: string;
     newStart: string;
@@ -14,21 +12,24 @@ const startSubstitutions: StartSubstitution[] = [
         newStart: '##[debug] ',
     },
     {
-        oldStart: '\u001B[32mINFO\u001b[39m ', // The initial characters are escape characters used for color formatting
+        oldStart: '\u001B[32mINFO\u001b[39m ', // Includes escape characters used for color formatting
         newStart: '##[debug]',
     },
 ];
 
 export const stdoutTransformer = (rawData: string): string => {
     for (const startSubstitution of startSubstitutions) {
-        const newData = makeSubstitutionAtStart(rawData, startSubstitution.oldStart, startSubstitution.newStart);
-        if (newData) return newData;
+        const newData = applySubstitutionAtStart(rawData, startSubstitution.oldStart, startSubstitution.newStart);
+
+        if (newData) {
+            return newData;
+        }
     }
 
     return rawData;
 };
 
-function makeSubstitutionAtStart(rawData: string, oldStart: string, newStart: string): string | null {
+function applySubstitutionAtStart(rawData: string, oldStart: string, newStart: string): string | null {
     if (rawData.startsWith(oldStart)) {
         return newStart + rawData.substring(oldStart.length);
     }
