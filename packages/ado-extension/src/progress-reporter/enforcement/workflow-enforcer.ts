6add19667212bedf2f43ea49e55e63d1ba3f9df2
Tania Martinez Villagomez <taniamar@microsoft.com>
2022-03-22 10:50:08 -0700

    Fixing logic problems

--- packages/ado-extension/src/progress-reporter/enforcement/workflow-enforcer.ts
+++ packages/ado-extension/src/progress-reporter/enforcement/workflow-enforcer.ts
@@ -36,7 +36,7 @@ export class WorkflowEnforcer extends ProgressReporter {
     }
 
     private async failIfMutuallyExclusiveParametersWereConfigured(): Promise<boolean> {
-        if (this.adoTaskConfig.getUrl() && this.adoTaskConfig.getSiteDir()) {
+        if (typeof this.adoTaskConfig.getUrl() !== 'undefined' && typeof this.adoTaskConfig.getSiteDir() !== 'undefined') {
             this.logger.logError('A configuration error has occurred, Url and SiteDire inputs cannot be set at the same time');
             await this.failRun();
             return true;
