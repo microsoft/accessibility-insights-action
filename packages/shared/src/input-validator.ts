913c34818fefc4137412294e6a71e42b7731360b
Tania Martinez Villagomez <taniamar@microsoft.com>
2022-04-21 15:54:29 -0700

    Fixing format and copyright problems

--- packages/shared/src/input-validator.ts
+++ packages/shared/src/input-validator.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 import { injectable, inject } from 'inversify';
 import { iocTypes } from './ioc/ioc-types';
 import { Logger } from './logger/logger';
