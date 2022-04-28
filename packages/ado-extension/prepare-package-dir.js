1f18d07ed20b2614e21b1385ca711253ef930867
Dan Bjorge <danielbj@microsoft.com>
2022-04-28 13:21:07 -0400

    Apply new resolution logic to github action too

--- packages/ado-extension/prepare-package-dir.js
+++ packages/ado-extension/prepare-package-dir.js
@@ -1,17 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// run from root folder, this scripts prepares the dist folder
-
-const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const packageJson = require(process.cwd() + '/package.json');
-const sharedPackageJson = require(path.resolve(process.cwd(), '../shared/package.json'));
-const rootPackageJson = require(path.resolve(process.cwd(), '../../package.json'));
 const getWebpackConfig = require(process.cwd() + '/webpack.config');
-
-// This is for forward compat; the latter was deprecated in favor of the former in Node 14
-const rmdirSync = fs.rmSync || fs.rmdirSync;
+const { buildRuntimePackageMetadata } = require('../shared/build-runtime-package-metadata');
 
 // We allow overrides of extension/task identifiers so we can deploy
 // different versions of the extension with the same YAML
@@ -35,32 +28,6 @@ if (overrideTaskId && overrideTaskId.length > 0) {
     taskJson.id = overrideTaskId;
 }
 
-const packageDependencies = packageJson.dependencies;
-const webpackConfig = getWebpackConfig();
-const webpackExternals = webpackConfig.externals ? webpackConfig.externals : [];
-const imagePackageDependencies = {};
-
-webpackExternals.forEach((packageName) => {
-    if (packageDependencies.hasOwnProperty(packageName)) {
-        imagePackageDependencies[packageName] = packageDependencies[packageName];
-    } else if (sharedPackageJson.dependencies.hasOwnProperty(packageName)) {
-        imagePackageDependencies[packageName] = sharedPackageJson.dependencies[packageName];
-    } else {
-        throw new Error(`Package '${packageName}' is not declared in package.json dependencies{} section.`);
-    }
-});
-
-const newPackageJson = {
-    ...packageJson,
-    scripts: undefined,
-    dependencies: imagePackageDependencies,
-    devDependencies: undefined,
-    resolutions: rootPackageJson.resolutions,
-};
-
-fs.writeFileSync('dist/pkg/package.json', JSON.stringify(newPackageJson, undefined, 4));
-console.log('wrote package.json to dist/pkg/package.json');
-
 console.log(JSON.stringify(extensionJson, null, 4));
 fs.writeFileSync('dist/ado-extension.json', JSON.stringify(extensionJson));
 console.log('copied ado-extension.json to dist/pkg/ado-extension.json with any overrides');
@@ -75,21 +42,10 @@ console.log('copied ado-extension-overview.md to dist/overview.md');
 fs.copyFileSync('../../icons/brand-blue-48px.png', 'dist/pkg/extension-icon.png');
 console.log('copied brand-blue-48px.png to dist/pkg/extension-icon.png');
 
-fs.copyFileSync('../../yarn.lock', 'dist/pkg/yarn.lock');
-console.log('copied yarn.lock to dist/pkg/yarn.lock');
-
-console.log('updating yarn.lock based on prepared package.json');
-// This intentionally uses execSync rather than the normally-preferred execFileSync
-// because it relies on shell behavior to pick whether to invoke yarn, yarn.exe,
-// yarn.bat, or yarn.cmd (any of these are possible depending on OS + how Yarn is
-// installed). This doesn't create a shell injection concern because the command
-// is a fixed string.
-execSync('yarn install --prod --ignore-engines --ignore-scripts', {
-    stdio: 'inherit',
-    cwd: path.join(__dirname, 'dist', 'pkg'),
+buildRuntimePackageMetadata({
+    packageJson,
+    webpackConfig: getWebpackConfig(),
+    outputDirectory: path.join('.', 'dist', 'pkg'),
 });
-
-console.log('removing node_modules left behind by yarn.lock update');
-rmdirSync('dist/pkg/node_modules', { recursive: true });
 
 console.log('prepare-package-dir complete');
