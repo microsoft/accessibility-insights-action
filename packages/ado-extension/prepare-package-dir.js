4a5b2854abeb6b4f358802da5dfff6a4068ae32b
Dan Bjorge <danielbj@microsoft.com>
2022-04-28 14:39:19 -0400

    fix: apply yarn resolutions at runtime (#1162)
    #### Details
    
    This PR updates both the ADO and GHA build processes to include our yarn resolutions in the package.json files we generate for the purposes of runtime dependency installation.
    
    It also fixes a few incidental issues that were discovered as part of making that change:
    * Unblocks `run-locally.js` by ensuring `AGENT_TEMPDIRECTORY` is set (required after #1141)
    * Updates the resolution for `accessibility-insights-report` to pin it to the version we had actually been using at runtime (4.2.2); updating to 4.3.0 will come separately with the same change that updates to v1.x of the scan package
    * Updates the resolution for `underscore` to pin it to 1.12.1 exactly, to match https://github.com/apify/apify-js/pull/991 (thanks @karanbirsingh for debugging this)
    
    ##### Motivation
    
    The immediate motivation is to work around a new issue where the task would fail at runtime during its install dependencies step complaining that it required a lockfile update.
    
    The more important motivation is that we really should have been applying these resolutions the whole time; it's important that we apply them consistently at runtime vs build/test time, and wildly lucky that not doing so hasn't broken anything before this point.
    
    This also has the nice side benefit that it resolves most of the warning spew in the "installing runtime dependencies" log group of the ADO task's output (it turns out those warnings were mostly legitimate)
    
    ##### Context
    
    I chose not to do a deep audit of all the resolutions that weren't immediately breaking; most of them become obsolete in the very near future when we'll pick up v1.x of the scan package, so it didn't seem worth the engineering effort.
    
    #### Pull request checklist
    <!-- If a checklist item is not applicable to this change, write "n/a" in the checkbox -->
    - [n/a] Addresses an existing issue: Fixes #0000
    - [n/a] Added relevant unit test for your changes. (`yarn test`)
    - [n/a] Verified code coverage for the changes made. Check coverage report at: `<rootDir>/test-results/unit/coverage`
    - [x] Ran precheckin (`yarn precheckin`)
    - [x] Described how this PR impacts both the ADO extension and the GitHub action

--- packages/ado-extension/prepare-package-dir.js
+++ packages/ado-extension/prepare-package-dir.js
@@ -1,12 +1,10 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// run from root folder, this scripts prepares the dist folder
-
 const fs = require('fs');
 const path = require('path');
 const packageJson = require(process.cwd() + '/package.json');
-const sharedPackageJson = require(path.resolve(process.cwd(), '../shared/package.json'));
 const getWebpackConfig = require(process.cwd() + '/webpack.config');
+const { buildRuntimePackageMetadata } = require('../shared/build-runtime-package-metadata');
 
 // We allow overrides of extension/task identifiers so we can deploy
 // different versions of the extension with the same YAML
@@ -30,31 +28,6 @@ if (overrideTaskId && overrideTaskId.length > 0) {
     taskJson.id = overrideTaskId;
 }
 
-const packageDependencies = packageJson.dependencies;
-const webpackConfig = getWebpackConfig();
-const webpackExternals = webpackConfig.externals ? webpackConfig.externals : [];
-const imagePackageDependencies = {};
-
-webpackExternals.forEach((packageName) => {
-    if (packageDependencies.hasOwnProperty(packageName)) {
-        imagePackageDependencies[packageName] = packageDependencies[packageName];
-    } else if (sharedPackageJson.dependencies.hasOwnProperty(packageName)) {
-        imagePackageDependencies[packageName] = sharedPackageJson.dependencies[packageName];
-    } else {
-        throw new Error(`Package '${packageName}' is not declared in package.json dependencies{} section.`);
-    }
-});
-
-const newPackageJson = {
-    ...packageJson,
-    scripts: undefined,
-    dependencies: imagePackageDependencies,
-    devDependencies: undefined,
-};
-
-fs.writeFileSync('dist/pkg/package.json', JSON.stringify(newPackageJson, undefined, 4));
-console.log('copied package.json to dist/pkg/package.json');
-
 console.log(JSON.stringify(extensionJson, null, 4));
 fs.writeFileSync('dist/ado-extension.json', JSON.stringify(extensionJson));
 console.log('copied ado-extension.json to dist/pkg/ado-extension.json with any overrides');
@@ -63,11 +36,16 @@ console.log(JSON.stringify(taskJson, null, 4));
 fs.writeFileSync('dist/pkg/task.json', JSON.stringify(taskJson));
 console.log('copied task.json to dist/pkg/task.json with any overrides');
 
-fs.copyFileSync('../../yarn.lock', 'dist/pkg/yarn.lock');
-console.log('copied yarn.lock to dist/pkg/yarn.lock');
-
 fs.copyFileSync('../../docs/ado-extension-overview.md', 'dist/overview.md');
 console.log('copied ado-extension-overview.md to dist/overview.md');
 
 fs.copyFileSync('../../icons/brand-blue-48px.png', 'dist/pkg/extension-icon.png');
 console.log('copied brand-blue-48px.png to dist/pkg/extension-icon.png');
+
+buildRuntimePackageMetadata({
+    packageJson,
+    webpackConfig: getWebpackConfig(),
+    outputDirectory: path.join('.', 'dist', 'pkg'),
+});
+
+console.log('prepare-package-dir complete');
