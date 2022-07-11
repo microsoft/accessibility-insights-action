0e0ef00bf1320c8ca8d8d67e2e15fc2952c62b01
a11y-insights <a11y-insights-team@microsoft.com>
2022-07-11 17:22:02 +0000

    root commit

--- packages/gh-action/jest.config.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-const baseConfig = require('../../jest.config.base');
-const package = require('./package');
-
-module.exports = {
-    ...baseConfig,
-    displayName: package.name,
-};
--- packages/shared/src/telemetry/telemetry-client.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-import { TelemetryEvent } from './telemetry-event';
-
-export interface TelemetryClient {
-    trackEvent(event: TelemetryEvent): void;
-    flush(): Promise<void>;
-}
