cb6126c80998c455353b512bf24a48d290aaa415
Katy DeCorah <katydecorah@microsoft.com>
2022-09-06 11:03:17 -0400

    Add test coverage for setResult
    Update ado-extension-test.spec.ts

--- packages/ado-extension/src/ado-extension-test.spec.ts
+++ packages/ado-extension/src/ado-extension-test.spec.ts
@@ -1,8 +1,46 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-describe('ADO Extension test infrastructure', () => {
-    it('placeholder test in ADO extension wrapper package', () => {
-        expect(0).toBe(0);
+import { runScan } from './ado-extension';
+import * as adoTask from 'azure-pipelines-task-lib/task';
+
+let scanResponse: Promise<any>;
+
+jest.mock('./ioc/setup-ioc-container', () => ({
+    setupIocContainer: jest.fn().mockImplementation(() => ({
+        get: jest.fn().mockImplementation(() => ({
+            setup: jest.fn().mockResolvedValue(undefined),
+            scan: jest.fn().mockImplementationOnce(() => scanResponse),
+        })),
+    })),
+}));
+
+jest.mock('azure-pipelines-task-lib/task');
+
+describe('runScan', () => {
+    it.each`
+        scanResult | expectedCode                    | expectedMessage
+        ${true}    | ${adoTask.TaskResult.Succeeded} | ${'Scan completed successfully'}
+        ${false}   | ${adoTask.TaskResult.Failed}    | ${'Scan completed with errors. To see all failures and scan details, visit the Extensions tab to download the accessibility report.'}
+    `(`show '$expectedMessage' when scan returns '$scanResult'`, async ({ scanResult, expectedCode, expectedMessage }) => {
+        scanResponse = Promise.resolve(scanResult);
+        const setResultMock = jest.spyOn(adoTask, 'setResult');
+        runScan();
+        await flushPromises();
+        expect(setResultMock).toBeCalledWith(expectedCode, expectedMessage);
+    });
+
+    it('show exception message when runScan experiences an error', async () => {
+        scanResponse = Promise.reject(new Error('test error'));
+        const setResultMock = jest.spyOn(adoTask, 'setResult');
+        runScan();
+        await flushPromises();
+        expect(setResultMock).toBeCalledWith(adoTask.TaskResult.Failed, 'Exception thrown in extension: test error');
     });
 });
+
+// runScan is a synchronous function with a nested self-invoking async function,
+// flushPromises waits for the async function to complete
+function flushPromises() {
+    return new Promise((resolve) => setImmediate(resolve));
+}
