91e6ffac248b5804ea3b9fdcfd22261ae7166d47
a11y-insights <a11y-insights-team@microsoft.com>
2022-10-03 17:45:53 +0000

    root commit

--- packages/shared/src/exit-code.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-export const ExitCode = {
-    ScanCompletedNoUserActionIsNeeded: 0,
-    ScanCompletedUserActionIsNeeded: 1,
-    ScanFailedToComplete: 2,
-};
--- packages/shared/src/logger/base-telemetry-properties.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-export interface BaseTelemetryProperties {
-    source: string;
-    [index: string]: string;
-}
