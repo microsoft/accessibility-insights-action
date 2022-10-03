8027613365947924acd099a9db35f4fc4966548a
a11y-insights <a11y-insights-team@microsoft.com>
2022-10-03 14:39:26 +0000

    root commit

--- packages/shared/src/baseline-info.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-import { BaselineEvaluation } from 'accessibility-insights-scan';
-
-export type BaselineInfo = {
-    baselineFileName?: string;
-    baselineEvaluation?: BaselineEvaluation;
-};
