4b4e0bf059e90aae94c6bc128bde43a4b02f6e43
a11y-insights <a11y-insights-team@microsoft.com>
2022-10-18 20:33:55 +0000

    root commit

--- packages/ado-extension/src/ioc/ado-ioc-types.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-export const AdoIocTypes = {
-    AdoTask: 'AdoTask',
-    NodeApi: 'NodeApi',
-    AppInsights: 'AppInsights',
-};
