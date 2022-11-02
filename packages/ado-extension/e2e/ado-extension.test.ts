5297d139727eaa2805746b7c1ac6010d7f9b531f
Katy DeCorah <katydecorah@microsoft.com>
2022-11-02 14:49:27 -0400

    Draft

--- packages/ado-extension/e2e/ado-extension.test.ts
+++ packages/ado-extension/e2e/ado-extension.test.ts
@@ -2,25 +2,28 @@
 // Licensed under the MIT License.
 
 import * as path from 'path';
-import * as assert from 'assert';
 import * as ttm from 'azure-pipelines-task-lib/mock-test';
 
-describe('Sample task tests', function () {
-    it('should succeed with simple inputs', function (done: Mocha.Done) {
+describe('Sample task tests', () => {
+    it('should succeed with simple inputs', (done) => {
         const compiledSourcePath = path.join(__dirname, 'run.js');
-
         const testSubject: ttm.MockTestRunner = new ttm.MockTestRunner(compiledSourcePath);
 
         testSubject.run();
-        console.log(testSubject.succeeded);
-        assert.strictEqual(testSubject.succeeded, true, 'should have succeeded');
-        assert.strictEqual(testSubject.warningIssues.length, 0, 'should have no warnings');
-        assert.strictEqual(testSubject.errorIssues.length, 0, 'should have no errors');
-        assert.strictEqual(
-            testSubject.stdOutContained('https://www.washington.edu/accesscomputing/AU/before.html'),
-            true,
-            'should display the input url',
-        );
+
+        // Uncomment the following line to debug e2e tests:
+        // console.log(testSubject);
+
+        expect(testSubject.succeeded).toBeTruthy();
+        expect(testSubject.warningIssues.length).toEqual(0);
+        expect(testSubject.errorIssues.length).toEqual(0);
+        expect(
+            testSubject.stdOutContained(
+                'Accessibility scanning of URL https://www.washington.edu/accesscomputing/AU/before.html completed',
+            ),
+        ).toBeTruthy();
+        expect(testSubject.stdOutContained('Rules: 4 with failures, 11 passed, 39 not applicable')).toBeTruthy();
+
         done();
     });
 });
