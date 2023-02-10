10e840a52852ed0acda8c3abb0e07ccc9dd2f7ad
Katy DeCorah <katydecorah@microsoft.com>
2023-02-10 15:55:14 -0500

    fix: Clip scan summary snapshot

--- packages/ado-extension/e2e/ado-extension.test.ts
+++ packages/ado-extension/e2e/ado-extension.test.ts
@@ -10,23 +10,13 @@ describe('Sample task tests', () => {
         inputs = {};
     });
 
-    it('returns expected markdown report', () => {
+    it('returns expected scan summary and footer', () => {
         inputs = {
             url: 'https://www.washington.edu/accesscomputing/AU/before.html',
         };
         const testSubject = runTestWithInputs(inputs);
         expect(filterStdOut(testSubject.stdout)).toMatchInlineSnapshot(`
-            "Accessibility Insights
-
-            20 failure instances
-            * (10) label:  Ensures every form element has a label
-            * (4) color-contrast:  Ensures the contrast between foreground and background colors meets WCAG 2 AA contrast ratio thresholds
-            * (3) link-in-text-block:  Ensure links are distinguished from surrounding text in a way that does not rely on color
-            * (2) image-alt:  Ensures <img> elements have alternate text or a role of none or presentation
-            * (1) html-has-lang:  Ensures every HTML document has a lang attribute
-
-            Baseline not configured
-            A baseline lets you mark known failures so it's easier to identify new failures as they're introduced. See baselining docs (https://aka.ms/ado-extension-usage-baseline) for more.-------------------
+            "-------------------
             Scan summary
             URLs: 1 with failures, 0 passed, 0 not scannable
             Rules: 5 with failures, 14 passed, 36 not applicable
@@ -38,7 +28,7 @@ describe('Sample task tests', () => {
         `);
 
         function filterStdOut(stdout: string) {
-            const logs = stdout.match(/Accessibility Insights\n(.|\n)*##\[debug\]\[Telemetry\] tracking a 'ScanCompleted' event/);
+            const logs = stdout.match(/-------------------(.|\n)*##\[debug\]\[Telemetry\] tracking a 'ScanCompleted' event/);
             return logs ? logs[0] : '';
         }
     });
