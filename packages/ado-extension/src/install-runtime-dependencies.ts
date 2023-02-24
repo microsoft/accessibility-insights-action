23b75eff05d37ac85a3ab4ab2dc573dd6f8e737a
Dan Bjorge <danielbj@microsoft.com>
2023-02-23 20:31:31 -0500

    update install-runtime-dependencies to use agent node16 and bundled yarn

--- packages/ado-extension/src/install-runtime-dependencies.ts
+++ packages/ado-extension/src/install-runtime-dependencies.ts
@@ -1,17 +1,24 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { execSync } from 'child_process';
+import { execFileSync } from 'child_process';
+import { argv } from 'process';
+import { readdirSync } from 'fs';
+import { join } from 'path';
 
 export function installRuntimeDependencies(): void {
     console.log('##[group]Installing runtime dependencies');
 
-    // This intentionally uses execSync rather than the normally-preferred execFileSync
-    // because it relies on shell behavior to pick whether to invoke yarn, yarn.exe,
-    // yarn.bat, or yarn.cmd (any of these are possible depending on OS + how Yarn is
-    // installed). This doesn't create a shell injection concern because the command
-    // is a fixed string.
-    execSync('yarn install --immutable', {
+    const nodePath = argv[0];
+
+    const yarnReleasesPath = join(__dirname, '.yarn', 'releases');
+    const yarnFilename = readdirSync(yarnReleasesPath)[0];
+    const yarnPath = join(yarnReleasesPath, yarnFilename);
+
+    console.log(`##[debug]Using node from ${nodePath}`);
+    console.log(`##[debug]Using bundled yarn from ${yarnPath}`);
+
+    execFileSync(nodePath, [yarnPath, 'install', '--immutable'], {
         stdio: 'inherit',
         cwd: __dirname,
     });
