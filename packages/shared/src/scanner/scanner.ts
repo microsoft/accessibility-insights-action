e5aec5227eb3ad7bff02913f8047f54ce600ea90
Dan Bjorge <danielbj@microsoft.com>
2023-04-28 17:49:40 -0400

    fix misleading log message when using default chromePath

--- packages/shared/src/scanner/scanner.ts
+++ packages/shared/src/scanner/scanner.ts
@@ -111,7 +111,7 @@ export class Scanner {
 
             this.logger.logStartGroup(`Scanning URL ${scanArguments.url}`);
             this.logger.logDebug(`Starting accessibility scanning of URL ${scanArguments.url}`);
-            this.logger.logDebug(`Chrome app executable: ${scanArguments.chromePath ?? 'system default'}`);
+            this.logger.logDebug(`Chrome app executable: ${scanArguments.chromePath ?? 'default (Puppeteer-bundled)'}`);
 
             const crawlerParameters = this.crawlerParametersBuilder.build(scanArguments);
 
