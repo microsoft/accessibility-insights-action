3f4381eeec84335b4dbd398b18cce502656fd6bd
madalynrose <madparker@microsoft.com>
2023-08-31 16:48:35 -0400

    fix license in wmic.d.ts

--- packages/ado-extension/src/wmic.d.ts
+++ packages/ado-extension/src/wmic.d.ts
@@ -1,8 +1,5 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 // #1575: This file is used to declare types for the wmi-client module that doesn't have type declarations.
 // This module is used in install-runtime-dependencies.ts only on windows clients to check if
 // the platform has the necessary dependencies to run Chrome.
