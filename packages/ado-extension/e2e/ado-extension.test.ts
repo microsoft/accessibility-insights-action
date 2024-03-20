fcce2398a6397662e9578513c5736b6a08c0e4f7
Saanica Ghate <v-sghate@microsoft.com>
2024-03-20 16:42:24 +0530

    updated version check regex.

--- packages/ado-extension/e2e/ado-extension.test.ts
+++ packages/ado-extension/e2e/ado-extension.test.ts
@@ -23,7 +23,7 @@ describe('Sample task tests', () => {
         expect(testSubject.stdOutContained("##[debug][Telemetry] tracking a 'ScanCompleted' event"));
 
         function filterStdOut(stdout: string) {
-            const logs = stdout.match(/-------------------(.|\n)*This scan used axe-core 4\.7\.2/);
+            const logs = stdout.match(/-------------------(.|\n)*This scan used axe-core 4\.8\.4/);
             return logs ? logs[0] : '';
         }
     });
