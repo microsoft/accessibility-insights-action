e2f0f306e35ccc1136631f62bbd207ad86044f10
Jeevani Chinthala <v-jeevanic@microsoft.com>
2024-10-17 18:45:02 +0530

    ado-extension.test update

--- packages/ado-extension/e2e/ado-extension.test.ts
+++ packages/ado-extension/e2e/ado-extension.test.ts
@@ -23,7 +23,7 @@ describe('Sample task tests', () => {
         expect(testSubject.stdOutContained("##[debug][Telemetry] tracking a 'ScanCompleted' event"));
 
         function filterStdOut(stdout: string) {
-            const logs = stdout.match(/-------------------(.|\n)*This scan used axe-core 4\.10\.0/);
+            const logs = stdout.match(/-------------------(.|\n)*This scan used axe-core 4\.10\.1/);
             return logs ? logs[0] : '';
         }
     });
