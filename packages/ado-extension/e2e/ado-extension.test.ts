f31c599bae07d0af6ef68bfdb60d8c4dfd4e9e89
Jitendra Jain (TATA CONSULTANCY SERVICES LTD) <v-jitendjain@microsoft.com>
2026-02-13 16:12:34 +0530

    updated the URLs

--- packages/ado-extension/e2e/ado-extension.test.ts
+++ packages/ado-extension/e2e/ado-extension.test.ts
@@ -30,7 +30,7 @@ describe('Sample task tests', () => {
 
     it('should succeed with simple inputs', () => {
         inputs = {
-            url: 'https://www.washington.edu/accesscomputing/AU/before.html',
+            url: 'https://projects.accesscomputing.uw.edu/au/before.html',
         };
         const testSubject = runTestWithInputs(inputs);
 
@@ -38,7 +38,7 @@ describe('Sample task tests', () => {
         expect(testSubject.errorIssues.length).toEqual(1);
         expect(
             testSubject.stdOutContained(
-                'Accessibility scanning of URL https://www.washington.edu/accesscomputing/AU/before.html completed',
+                'Accessibility scanning of URL https://projects.accesscomputing.uw.edu/au/before.html completed',
             ),
         ).toBeTruthy();
         expect(testSubject.stdOutContained('Rules: 5 with failures, 13 passed, 36 not applicable')).toBeTruthy();
@@ -74,7 +74,7 @@ describe('Sample task tests', () => {
     it('only scans the inputUrls when maxUrls input matches number of inputUrls, (url input can be anything)', () => {
         inputs = {
             url: 'https://www.washington.edu', // this input must be set to something, but it is ignored
-            inputUrls: 'https://www.washington.edu/accesscomputing/AU/before.html https://www.washington.edu/accesscomputing/AU/after.html',
+            inputUrls: 'https://projects.accesscomputing.uw.edu/au/before.html https://projects.accesscomputing.uw.edu/au/after.html',
             maxUrls: '2', //By setting `maxUrls` to 2, only the `inputUrls` will be scanned
         };
         const testSubject = runTestWithInputs(inputs);
@@ -83,12 +83,12 @@ describe('Sample task tests', () => {
         expect(testSubject.errorIssues.length).toEqual(1);
         expect(
             testSubject.stdOutContainedRegex(
-                new RegExp('Processing loaded page.*{"url":"https://www.washington.edu/accesscomputing/AU/before.html"}'),
+                new RegExp('Processing loaded page.*{"url":"https://projects.accesscomputing.uw.edu/au/before.html"}'),
             ),
         ).toBeTruthy();
         expect(
             testSubject.stdOutContainedRegex(
-                new RegExp('Processing loaded page.*{"url":"https://www.washington.edu/accesscomputing/AU/after.html"}'),
+                new RegExp('Processing loaded page.*{"url":"https://projects.accesscomputing.uw.edu/au/after.html"}'),
             ),
         ).toBeTruthy();
         expect(testSubject.stdOutContained('URLs: 1 with failures, 1 passed, 0 not scannable')).toBeTruthy();
@@ -145,7 +145,7 @@ describe('Sample task tests', () => {
 
     it('should fail if both URL and staticSiteDir are defined', () => {
         inputs = {
-            url: 'https://www.washington.edu/accesscomputing/AU/before.html',
+            url: 'https://projects.accesscomputing.uw.edu/au/before.html',
             staticSiteDir: path.join(__dirname, '..', '..', '..', 'dev', 'website-root'),
         };
         const testSubject = runTestWithInputs(inputs);
